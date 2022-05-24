5390ef301c46073341eaa1fa64a188661463c69a
Vadim <registiy@gmail.com>
2022-05-24 21:44:14 +0300

    Initial Commit

---
+++ JsonApp/JsonAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  JsonAppApp.swift
+//  JsonApp
+//
+//  Created by Vadim on 24.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct JsonAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
